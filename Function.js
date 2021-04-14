b986da76420fdf241f8657025c7ab22668fa799d
mahsakw80 <mahsa.kin80@gmail.com>
2021-04-14 13:53:39 -0700

    secend

---
+++ Function.js
@@ -0,0 +1,35 @@
+function Hello(){
+    alert ('Hi!  Welcome')
+
+}
+
+
+function onmusepy(elm){
+    elm.src="./download (3).jpg"
+
+}
+function outmusepy(elm){
+    elm.src="./p1.jpg"
+
+}
+
+function onmusejs(elm){
+    elm.src="./j2.jpg"
+
+}
+function outmusejs(elm){
+    elm.src="./j1.jpg"
+
+}
+
+function onmusec(elm){
+    elm.src="./c2.jpg"
+    Element.style.borderRadius='10%'
+
+}
+function outmusec(elm){
+    elm.src="./c1.jpg"
+    
+
+}
+
